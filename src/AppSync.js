b72169f9bc9d89e3eb1c433b1b84b54f7b537b82
fancykarl <153137193+fancykarl@users.noreply.github.com>
2024-08-19 00:54:59 +0200

    Delete src/AppSync.js

--- src/AppSync.js
+++
@@ -1,6 +0,0 @@
-export default {
-    'graphqlEndpoint': 'https://psmtiwoklrgkjhfwx5rapn6jsi.appsync-api.eu-central-1.amazonaws.com/graphql',
-    "region": "eu-central",
-    "authenticationType": "API_KEY",
-    "apiKey": "da2-dd6bcswj25hjzntowezq6kte7q"
-}
